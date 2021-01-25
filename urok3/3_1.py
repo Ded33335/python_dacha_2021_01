d99b0bb3a7af5fddc8512130f469c3a2982706ce
Ded33335 <ded33335@mail.ru>
2021-01-25 12:42:18 +0300

    first file whith pygame

---
+++ urok3/3_1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 25 12:23:03 2021
+
+@author: ded
+"""
+import pygame
+from pygame.draw import *
+
+pygame.init()
+
+FPS = 30
+screen = pygame.display.set_mode((400, 400))
+
+rect(screen, (255, 0, 255), (100, 100, 200, 200))
+rect(screen, (0, 0, 255), (100, 100, 200, 200), 5)
+polygon(screen, (255, 255, 0), [(100,100), (200,50),
+                               (300,100), (100,100)])
+polygon(screen, (0, 0, 255), [(100,100), (200,50),
+                               (300,100), (100,100)], 5)
+circle(screen, (0, 255, 0), (200, 175), 50)
+circle(screen, (255, 255, 255), (200, 175), 50, 5)
+
+pygame.display.update()
+clock = pygame.time.Clock()
+finished = False
+
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+
+pygame.quit()
